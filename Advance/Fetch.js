9984ad2e25f7129e9d1cd4739ab17445c63b36ff
muhammadnaseer44 <naseerbaloch110@outlook.com>
2024-03-23 09:59:47 +0500

    edit fetch

--- Advance/Fetch.js
+++ Advance/Fetch.js
@@ -1,33 +1,33 @@
 // Fetch Method 1
-// fetch('https://jsonplaceholder.typicode.com/users')
-// .then((response) => {
-//     return response.json()
-// })
-// .then((data) => {
-//     console.log(data);
-// })
-// .catch((error) => console.log(error));
+fetch('https://jsonplaceholder.typicode.com/users/1')
+    .then((response) => {
+        return response.json()
+    })
+    .then((data) => {
+        console.log(data);
+    })
+    .catch((error) => console.log(error));
 
 // Fetch Method 2
 // fetch('https://jsonplaceholder.typicode.com/users')
-// .then((res) => res.json())
-// .then((data) => console.log(data)) 
+//     .then((res) => res.json())
+//     .then((data) => console.log(data))
 
 // Fetch Method 3
 // fetch('https://jsonplaceholder.typicode.com/users')
-// .then((res) => res.json())
-// .then((data) => console.log(data[0])) 
+//     .then((res) => res.json())
+//     .then((data) => console.log(data[0]))
 
 // Fetch Method 4
 // fetch('https://jsonplaceholder.typicode.com/users')
-// .then((res) => res.json())
-// .then((data) => console.log(data[0].id)) 
+//     .then((res) => res.json())
+//     .then((data) => console.log(data[0].id))
 
 // Fetch Method 5
-fetch('https://jsonplaceholder.typicode.com/users')
-    .then((res) => res.json())
-    .then((data) => {
-        data.map((user) => {
-            console.log(user.id);
-        });
-    });
+// fetch('https://jsonplaceholder.typicode.com/users')
+//     .then((res) => res.json())
+//     .then((data) => {
+//         data.map((user) => {
+//             console.log(user.id);
+//         });
+//     });
